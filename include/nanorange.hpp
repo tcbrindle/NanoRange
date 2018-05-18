7dc11458b9eab2bca0ed041686276741053ad459
Tristan Brindle <t.c.brindle@gmail.com>
2018-05-18 17:59:13 +0100

    Turn mismatch into a CPO
    Now with deprection warnings

--- include/nanorange.hpp
+++ include/nanorange.hpp
@@ -38,6 +38,13 @@ inline namespace ranges {
 #define NANO_MSVC_NO_POISON_PILLS
 #endif
 
+#ifdef NANORANGE_NO_DEPRECATION_WARNINGS
+#define NANO_DEPRECATED
+#define NANO_DEPRECATED_FOR(x)
+#else
+#define NANO_DEPRECATED [[deprecated]]
+#define NANO_DEPRECATED_FOR(x) [[deprecated(x)]]
+#endif
 
 #define NANO_CONCEPT constexpr bool
 
@@ -3052,83 +3059,111 @@ NANO_INLINE_VAR(detail::count_fn, count)
 
 // [range.mismatch]
 
+namespace detail {
+
 // FIXME: Use tagged pair
-// N.B Three-legged forms are deprecated
-template <typename I1, typename S1, typename I2,
-          typename Proj1 = identity, typename Proj2 = identity,
-          typename Pred = equal_to<>>
-constexpr
-std::enable_if_t<
-    InputIterator<I1> &&
-    Sentinel<S1, I1> &&
-    InputIterator<I2> &&
-    IndirectRelation<Pred, projected<I1, Proj1>, projected<I2, Proj2>>,
-    std::pair<I1, I2>>
-mismatch(I1 first1, S1 last1, I2 first2, Pred pred = Pred{},
-         Proj1 proj1 = Proj1{}, Proj2 proj2 = Proj2{})
-{
-    while (first1 != last1 &&
-           nano::invoke(pred, nano::invoke(proj1, *first1), nano::invoke(proj2, *first2)))
+struct mismatch_fn {
+private:
+    template <typename I1, typename S1, typename I2,
+              typename Proj1, typename Proj2, typename Pred>
+    static constexpr std::pair<I1, I2>
+    impl3(I1 first1, S1 last1, I2 first2, Pred pred, Proj1 proj1, Proj2 proj2)
     {
-        ++first1; ++first2;
+        while (first1 != last1 &&
+                nano::invoke(pred, nano::invoke(proj1, *first1), nano::invoke(proj2, *first2)))
+        {
+            ++first1; ++first2;
+        }
+
+        return {first1, first2};
     }
 
-    return {first1, first2};
-}
-
-template <typename Rng1, typename I2, typename Proj1 = identity,
-          typename Proj2 = identity, typename Pred = equal_to<>>
-constexpr
-std::enable_if_t<
-    InputRange<Rng1> &&
-    InputIterator<I2> &&
-    IndirectRelation<Pred, projected<iterator_t<Rng1>, Proj1>, projected<I2, Proj2>>,
-    std::pair<safe_iterator_t<Rng1>, I2>>
-mismatch(Rng1&& rng1, I2 first2, Pred pred = Pred{}, Proj1 proj1 = Proj1{}, Proj2 proj2 = Proj2{})
-{
-    return nano::mismatch(
-            nano::begin(rng1), nano::end(rng1), std::move(first2),
-            std::move(pred), std::move(proj1), std::move(proj2));
-}
-
-template <typename I1, typename S1, typename I2, typename S2,
-          typename Proj1 = identity, typename Proj2 = identity,
-          typename Pred = equal_to<>>
-constexpr
-std::enable_if_t<
-    InputIterator<I1> &&
-    Sentinel<S1, I1> &&
-    InputIterator<I2> &&
-    Sentinel<S2, I2> &&
-    IndirectRelation<Pred, projected<I1, Proj1>, projected<I2, Proj2>>,
-    std::pair<I1, I2>>
-mismatch(I1 first1, S1 last1, I2 first2, S2 last2,
-         Pred pred = Pred{}, Proj1 proj1 = Proj1{}, Proj2 proj2 = Proj2{})
-{
-    while (first1 != last1 && first2 != last2 &&
-           nano::invoke(pred, nano::invoke(proj1, *first1), nano::invoke(proj2, *first2)))
+    template <typename I1, typename S1, typename I2, typename S2,
+              typename Proj1, typename Proj2, typename Pred>
+    static constexpr std::pair<I1, I2>
+    impl4(I1 first1, S1 last1, I2 first2, S2 last2, Pred pred, Proj1 proj1, Proj2 proj2)
     {
-        ++first1; ++first2;
+        while (first1 != last1 && first2 != last2 &&
+               nano::invoke(pred, nano::invoke(proj1, *first1), nano::invoke(proj2, *first2)))
+        {
+            ++first1; ++first2;
+        }
+
+        return {first1, first2};
     }
 
-    return {first1, first2};
+public:
+    template <typename I1, typename S1, typename I2,
+            typename Proj1 = identity, typename Proj2 = identity,
+            typename Pred = equal_to<>>
+    NANO_DEPRECATED
+    constexpr std::enable_if_t<
+        InputIterator<I1> &&
+        Sentinel<S1, I1> &&
+        InputIterator<I2> &&
+        IndirectRelation<Pred, projected<I1, Proj1>, projected<I2, Proj2>>,
+        std::pair<I1, I2>>
+    operator()(I1 first1, S1 last1, I2 first2, Pred pred = Pred{},
+               Proj1 proj1 = Proj1{}, Proj2 proj2 = Proj2{}) const
+    {
+        return mismatch_fn::impl3(std::move(first1), std::move(last1),
+                                  std::move(first2), std::move(pred),
+                                  std::move(proj1), std::move(proj2));
+    }
+
+    template <typename Rng1, typename I2, typename Proj1 = identity,
+              typename Proj2 = identity, typename Pred = equal_to<>>
+    NANO_DEPRECATED
+    constexpr std::enable_if_t<
+        InputRange<Rng1> &&
+        InputIterator<I2> &&
+        IndirectRelation<Pred, projected<iterator_t<Rng1>, Proj1>, projected<I2, Proj2>>,
+        std::pair<safe_iterator_t<Rng1>, I2>>
+    operator()(Rng1&& rng1, I2 first2, Pred pred = Pred{},
+               Proj1 proj1 = Proj1{}, Proj2 proj2 = Proj2{}) const
+    {
+        return mismatch_fn::impl3(
+                nano::begin(rng1), nano::end(rng1), std::move(first2),
+                std::move(pred), std::move(proj1), std::move(proj2));
+    }
+
+    template <typename I1, typename S1, typename I2, typename S2,
+            typename Proj1 = identity, typename Proj2 = identity,
+            typename Pred = equal_to<>>
+    constexpr std::enable_if_t<
+        InputIterator<I1> &&
+        Sentinel<S1, I1> &&
+        InputIterator<I2> &&
+        Sentinel<S2, I2> &&
+        IndirectRelation<Pred, projected<I1, Proj1>, projected<I2, Proj2>>,
+        std::pair<I1, I2>>
+    operator()(I1 first1, S1 last1, I2 first2, S2 last2,
+               Pred pred = Pred{}, Proj1 proj1 = Proj1{}, Proj2 proj2 = Proj2{}) const
+    {
+        return mismatch_fn::impl4(std::move(first1), std::move(last1),
+                                  std::move(first2), std::move(last2),
+                                  std::move(pred), std::move(proj1), std::move(proj2));
+    }
+
+    template <typename Rng1, typename Rng2, typename Proj1 = identity,
+              typename Proj2 = identity, typename Pred = equal_to<>>
+    constexpr std::enable_if_t<
+        InputRange<Rng1> &&
+        InputRange<Rng2> &&
+        IndirectRelation<Pred, projected<iterator_t<Rng1>, Proj1>, projected<iterator_t<Rng2>, Proj2>>,
+        std::pair<safe_iterator_t<Rng1>, safe_iterator_t<Rng2>>>
+    operator()(Rng1&& rng1, Rng2&& rng2, Pred pred = Pred{},
+               Proj1 proj1 = Proj1{}, Proj2 proj2 = Proj2{}) const
+    {
+        return mismatch_fn::impl4(
+                nano::begin(rng1), nano::end(rng1),
+                nano::begin(rng2), nano::end(rng2),
+                std::move(pred), std::move(proj1), std::move(proj2));
+    }
+};
 }
 
-template <typename Rng1, typename Rng2, typename Proj1 = identity,
-          typename Proj2 = identity, typename Pred = equal_to<>>
-constexpr
-std::enable_if_t<
-    InputRange<Rng1> &&
-    InputRange<Rng2> &&
-    IndirectRelation<Pred, projected<iterator_t<Rng1>, Proj1>, projected<iterator_t<Rng2>, Proj2>>,
-    std::pair<safe_iterator_t<Rng1>, safe_iterator_t<Rng2>>>
-mismatch(Rng1&& rng1, Rng2&& rng2, Pred pred = Pred{}, Proj1 proj1 = Proj1{}, Proj2 proj2 = Proj2{})
-{
-    return nano::mismatch(
-            nano::begin(rng1), nano::end(rng1),
-            nano::begin(rng2), nano::end(rng2),
-            std::move(pred), std::move(proj1), std::move(proj2));
-}
+NANO_INLINE_VAR(detail::mismatch_fn, mismatch)
 
 } // inline namespace ranges
 } // namespace nano
