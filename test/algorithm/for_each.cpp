ab64a4f7cb0f67235cea48f4695e2b9dbc3bcfa7
Tristan Brindle <t.c.brindle@gmail.com>
2018-05-22 04:28:32 +0100

    (Re)-enable for_each test

--- test/algorithm/for_each.cpp
+++ test/algorithm/for_each.cpp
@@ -10,41 +10,46 @@
 // Project home: https://github.com/ericniebler/range-v3
 
 #include <nanorange.hpp>
+#include <nanorange_extras.hpp>
 #include <vector>
 #include "../catch.hpp"
 
-namespace stl2 = nanorange;
+namespace stl2 = nano;
 
-struct S
-{
+namespace {
+
+struct S {
 	void p() const { *p_ += i_; }
+
 	int *p_;
 	int i_;
 };
+
+}
 
 TEST_CASE("[alg.for_each]")
 {
 	int sum = 0;
 	auto fun = [&](int i){ sum += i; };
 	std::vector<int> v1 { 0, 2, 4, 6 };
-	CHECK(stl2::for_each(v1.begin(), v1.end(), fun).in() == v1.end());
-	CHECK(stl2::for_each(v1, fun).in() == v1.end());
+	CHECK(stl2::for_each(v1.begin(), v1.end(), fun).first == v1.end());
+	CHECK(stl2::for_each(v1, fun).first == v1.end());
 	CHECK(sum == 24);
 
 	sum = 0;
 	auto rfun = [&](int& i){ sum += i; };
-	CHECK(stl2::for_each(v1.begin(), v1.end(), rfun).in() == v1.end());
-	CHECK(stl2::for_each(v1, rfun).in() == v1.end());
+	CHECK(stl2::for_each(v1.begin(), v1.end(), rfun).first == v1.end());
+	CHECK(stl2::for_each(v1, rfun).first == v1.end());
 	CHECK(sum == 24);
 
 	sum = 0;
 	std::vector<S> v2{{&sum, 0}, {&sum, 2}, {&sum, 4}, {&sum, 6}};
-	CHECK(stl2::for_each(v2.begin(), v2.end(), &S::p).in() == v2.end());
-	CHECK(stl2::for_each(v2, &S::p).in() == v2.end());
+	CHECK(stl2::for_each(v2.begin(), v2.end(), &S::p).first == v2.end());
+	CHECK(stl2::for_each(v2, &S::p).first == v2.end());
 	CHECK(sum == 24);
 
 	sum = 0;
-	CHECK(stl2::for_each(stl2::ext::make_range(v1.begin(), v1.end()), fun).in().get_unsafe() == v1.end());
+	CHECK(stl2::for_each(stl2::ext::make_range(v1.begin(), v1.end()), fun).first.get_unsafe() == v1.end());
 	CHECK(sum == 12);
 
 	{
@@ -59,10 +64,13 @@ TEST_CASE("[alg.for_each]")
 		auto result = stl2::for_each(std::move(il), [sum = 0](int i) mutable -> int {
 			return sum += i;
 		});
-		CHECK(result.fun()(0) == 12);
+		CHECK(result.second(0) == 12);
 	}
 
 	// Should compile
+    // FIXME MSVC
+#ifndef _MSC_VER
 	int matrix[3][4] = {};
 	stl2::for_each(matrix, [](int(&)[4]){});
+#endif
 }
