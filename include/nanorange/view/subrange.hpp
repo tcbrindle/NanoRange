024e2f8db14bd3afe899431058ad78effbcf71b2
Tristan Brindle <t.c.brindle@gmail.com>
2018-05-23 17:16:51 +0100

    Implement subrange
    It works for basic cases, tests could use a lot of work

--- include/nanorange/view/subrange.hpp
+++ include/nanorange/view/subrange.hpp
@@ -8,6 +8,7 @@
 #define NANORANGE_VIEW_SUBRANGE_HPP_INCLUDED
 
 #include <nanorange/view/interface.hpp>
+#include <nanorange/iterator.hpp>
 
 NANO_BEGIN_NAMESPACE
 
@@ -57,10 +58,10 @@ template <typename T, typename U>
 NANO_CONCEPT NotSameAs = !Same<remove_cvref_t<T>, remove_cvref_t<U>>;
 
 template <typename I, typename S, bool = SizedSentinel<S, I>>
-subrange_kind default_subrange_kind = subrange_kind::unsized;
+constexpr subrange_kind default_subrange_kind = subrange_kind::unsized;
 
 template <typename I, typename S>
-subrange_kind default_subrange_kind<I, S, true> = subrange_kind::sized;
+constexpr subrange_kind default_subrange_kind<I, S, true> = subrange_kind::sized;
 
 template <typename I, typename S, bool StoreSize = false>
 struct subrange_data {
@@ -96,43 +97,259 @@ public:
 
     subrange() = default;
 
-    constexpr subrange(I i, S s);
+    template <bool SS = StoreSize, typename = std::enable_if_t<!SS>>
+    constexpr subrange(I i, S s)
+        : data_{std::move(i), std::move(s)}
+    {}
 
-    constexpr subrange(I i, S s, difference_type_t<I> n);
+    template <subrange_kind KK = K, typename = std::enable_if_t<
+            KK == subrange_kind::sized>>
+    constexpr subrange(I i, S s, difference_type_t<I> n)
+        : data_{std::move(i), std::move(s), n}
+    {}
 
-    template <typename X, typename Y, subrange_kind Z>
-    constexpr subrange(subrange<X, Y, Z> r);
+    template <typename X, typename Y, subrange_kind Z, bool SS = StoreSize,
+              std::enable_if_t<
+                ConvertibleTo<X, I> &&
+                ConvertibleTo<Y, S> &&
+                Z == subrange_kind::sized &&
+                SS,
+              int> = 0>
+    constexpr subrange(subrange<X, Y, Z> r)
+        : subrange{r.begin(), r.end(), r.size()}
+    {}
 
-    template <typename X, typename Y, subrange_kind Z>
-    constexpr subrange(subrange<X, Y, Z> r, difference_type_t<I> n);
+    template <typename X, typename Y, subrange_kind Z, bool SS = StoreSize,
+            std::enable_if_t<
+            ConvertibleTo<X, I> &&
+            ConvertibleTo<Y, S> &&
+            !SS,
+            int> = 0>
+    constexpr subrange(subrange<X, Y, Z> r)
+            : subrange{r.begin(), r.end()}
+    {}
 
-    template <typename PairLike_>
-    constexpr subrange(PairLike_&& r);
+    template <typename X, typename Y, subrange_kind Z, subrange_kind KK = K,
+              std::enable_if_t<KK == subrange_kind::sized, int> = 0>
+    constexpr subrange(subrange<X, Y, Z> r, difference_type_t<I> n)
+        : subrange{r.begin(), r.end(), n}
+    {}
 
-    template <typename PairLike_>
-    constexpr subrange(PairLike_&& r, difference_type_t<I> n);
+    template <typename PairLike_, bool SS = StoreSize, std::enable_if_t<
+        detail::NotSameAs<PairLike_, subrange> &&
+        detail::PairlikeConvertibleTo<PairLike_, I, S> &&
+        !SS,
+    int> = 0>
+    constexpr subrange(PairLike_&& r)
+        : subrange{std::get<0>(std::forward<PairLike_>(r)),
+                   std::get<1>(std::forward<PairLike_>(r))}
+    {}
 
-    template <typename R>
-    constexpr subrange(R& r);
+    template <typename PairLike_, subrange_kind KK = K,
+              std::enable_if_t<
+                  detail::PairlikeConvertibleTo<PairLike_, I, S> &&
+                  KK == subrange_kind::sized,
+              int> = 0>
+    constexpr subrange(PairLike_&& r, difference_type_t<I> n)
+        : subrange{std::get<0>(std::forward<PairLike_>(r)),
+                   std::get<1>(std::forward<PairLike_>(r)),
+                   n}
+    {}
 
-    template <typename PairLike_>
-    constexpr operator PairLike_() const;
+    template <typename R, bool SS = StoreSize, std::enable_if_t<
+                  detail::NotSameAs<R, subrange> &&
+                  Range<R> &&
+                  ConvertibleTo<iterator_t<R>, I> &&
+                  ConvertibleTo<sentinel_t<R>, S> &&
+                  SS && SizedRange<R>,
+              int> = 0>
+    constexpr subrange(R& r)
+        : subrange{ranges::begin(r), ranges::end(r), distance(r)}
+    {}
 
-    constexpr I begin() const;
+    template <typename R, bool SS = StoreSize, std::enable_if_t<
+            detail::NotSameAs<R, subrange> &&
+            Range<R> &&
+            ConvertibleTo<iterator_t<R>, I> &&
+            ConvertibleTo<sentinel_t<R>, S> &&
+            !SS,
+            int> = 0>
+    constexpr subrange(R& r)
+            : subrange{ranges::begin(r), ranges::end(r)}
+    {}
 
-    constexpr S end() const;
+    template <typename PairLike_, std::enable_if_t<
+                detail::NotSameAs<PairLike_, subrange> &&
+                detail::PairLikeConvertibleFrom<PairLike_, const I&, const S&>,
+              int> = 0>
+    constexpr operator PairLike_() const
+    {
+        return PairLike_(begin(), end());
+    }
 
-    constexpr bool empty() const;
+    constexpr I begin() const
+    {
+        return data_.begin_;
+    }
 
-    constexpr difference_type_t<I> size() const;
+    constexpr S end() const
+    {
+        return data_.end_;
+    }
 
-    [[nodiscard]] constexpr subrange next(difference_type_t<I> n = 1) const;
+    NANO_NODISCARD constexpr bool empty() const
+    {
+        return data_.begin_ == data_.end_;
+    }
 
-    [[nodiscard]] constexpr subrange prev(difference_type_t<I> n = 1) const;
+    template <subrange_kind KK = K, bool SS = StoreSize>
+    constexpr auto size() const
+        -> std::enable_if_t<KK == subrange_kind::sized && SS,
+                            difference_type_t<I>>
+    {
+        return data_.size_;
+    }
 
-    constexpr subrange& advance(difference_type_t<I> n);
+    template <subrange_kind KK = K, bool SS = StoreSize>
+    constexpr auto size() const
+    -> std::enable_if_t<KK == subrange_kind::sized && !SS,
+            difference_type_t<I>>
+    {
+        return data_.end_ - data_.begin_;
+    }
+
+
+    NANO_NODISCARD constexpr subrange next(difference_type_t<I> n = 1) const
+    {
+        auto tmp = *this;
+        tmp.advance(n);
+        return tmp;
+    }
+
+    template <typename II = I>
+    NANO_NODISCARD constexpr auto prev(difference_type_t<I> n = 1) const
+        -> std::enable_if_t<BidirectionalIterator<I>, subrange>
+    {
+        auto tmp = *this;
+        tmp.advance(-n);
+        return tmp;
+    }
+
+    template <bool SS = StoreSize>
+    constexpr auto advance(difference_type_t<I> n)
+        -> std::enable_if_t<SS, subrange&>
+    {
+        data_.size_ -= n - ranges::advance(data_.begin_, n, data_.end_);
+        return *this;
+    }
+
+    template <bool SS = StoreSize>
+    constexpr auto advance(difference_type_t<I> n)
+    -> std::enable_if_t<!SS, subrange&>
+    {
+        ranges::advance(data_.begin_, n, data_.end_);
+        return *this;
+    }
 };
 
+#ifdef NANO_HAVE_DEDUCTION_GUIDES
+
+template <typename R, typename = std::enable_if_t<Range<R> && !SizedRange<R>>>
+subrange(R&) -> subrange<iterator_t<R>, sentinel_t<R>>;
+
+template <typename R, typename = std::enable_if_t<SizedRange<R>>>
+subrange(R&) -> subrange<iterator_t<R>, sentinel_t<R>, subrange_kind::sized>;
+
+#endif
+
+namespace detail {
+
+template <std::size_t, typename, typename, subrange_kind>
+struct subrange_get_helper;
+
+template <typename I, typename S, subrange_kind K>
+struct subrange_get_helper<0, I, S, K> {
+    constexpr I operator()(const subrange<I, S, K>& s) const
+    {
+        return s.begin();
+    }
+};
+
+template <typename I, typename S, subrange_kind K>
+struct subrange_get_helper<1, I, S, K> {
+    constexpr S operator()(const subrange<I, S, K>& s) const
+    {
+        return s.end();
+    }
+};
+
+}
+
+template <std::size_t N, typename I, typename S, subrange_kind K>
+constexpr auto get(const subrange<I, S, K>& r)
+    -> decltype(detail::subrange_get_helper<N, I, S, K>{}(r))
+{
+    return detail::subrange_get_helper<N, I, S, K>{}(r);
+}
+
+
+// Extensions for C++14 compilers without CTAD
+// These basically replicate the subrange constructors above
+
+template <typename I, typename S>
+constexpr auto make_subrange(I i, S s)
+    -> std::enable_if_t<Iterator<I> && Sentinel<S, I>,
+            decltype(subrange<I, S>{std::move(i), std::move(s)})>
+{
+    return {std::move(i), std::move(s)};
+}
+
+template <typename I, typename S>
+constexpr auto make_subrange(I i, S s, difference_type_t<I> n)
+    -> std::enable_if_t<Iterator<I> && Sentinel<S, I>,
+        decltype(subrange<I, S>{std::move(i), std::move(s)}, n)>
+{
+    return {std::move(i), std::move(s), n};
+}
+
+template <typename R>
+constexpr auto make_subrange(R& r)
+    -> std::enable_if_t<Range<R> && !SizedRange<R>,
+                        subrange<iterator_t<R>, sentinel_t<R>>>
+{
+    return {r};
+}
+
+template <typename R>
+constexpr auto make_subrange(R& r)
+-> std::enable_if_t<SizedRange<R>,
+                    subrange<iterator_t<R>, sentinel_t<R>, subrange_kind::sized>>
+{
+    return {r};
+}
+
 NANO_END_NAMESPACE
+
+namespace std {
+
+template <typename I, typename S, ::nano::subrange_kind K>
+class tuple_size<::nano::subrange<I, S, K>>
+    : public integral_constant<size_t, 2> {};
+
+template <typename I, typename S, ::nano::subrange_kind K>
+class tuple_element<0, ::nano::subrange<I, S, K>>
+{
+public:
+    using type = I;
+};
+
+template <typename I, typename S, ::nano::subrange_kind K>
+class tuple_element<1, ::nano::subrange<I, S, K>>
+{
+public:
+    using type = S;
+};
+
+}
 
 #endif
--- test/view/subrange.cpp
+++ test/view/subrange.cpp
@@ -1,38 +1,60 @@
-#include <stl2/iterator.hpp>
-#include "../simple_test.hpp"
+#include <nanorange/view/subrange.hpp>
+#include "../catch.hpp"
 
-using namespace __stl2;
+using namespace nano;
+namespace models = nano;
 
-int main() {
+TEST_CASE("view.subrange") {
 	{
-		using I = ext::subrange<int*, int*>;
-		using CI = ext::subrange<const int*, const int*>;
-		static_assert(models::View<I>);
-		static_assert(models::SizedRange<I>);
-		static_assert(models::ContiguousRange<I>);
-		static_assert(models::BoundedRange<I>);
-		static_assert(models::View<CI>);
-		static_assert(models::SizedRange<CI>);
-		static_assert(models::ContiguousRange<CI>);
-		static_assert(models::BoundedRange<CI>);
+		using I = nano::subrange<int*, int*>;
+		using CI = nano::subrange<const int*, const int*>;
+		static_assert(models::View<I>, "");
+		static_assert(models::SizedRange<I>, "");
+		// FIXME: ContiguousRange
+		//static_assert(models::ContiguousRange<I>, "");
+		static_assert(models::RandomAccessRange<I>, "");
+		static_assert(models::CommonRange<I>, "");
+		static_assert(models::View<CI>, "");
+		static_assert(models::SizedRange<CI>, "");
+		// FIXME: ContiguousRange
+		//static_assert(models::ContiguousRange<CI>, "");
+		static_assert(models::RandomAccessRange<CI>, "");
+		static_assert(models::CommonRange<CI>, "");
 	}
 
 	{
 		static constexpr int some_ints[] = {2, 3, 5, 7, 11, 13};
 		static constexpr std::size_t n = size(some_ints);
-		auto r = ext::subrange(some_ints + 0, some_ints + n);
+		auto r = make_subrange(some_ints + 0, some_ints + n);
 		using R = decltype(r);
-		static_assert(models::View<R>);
-		static_assert(models::SizedRange<R>);
-		static_assert(models::ContiguousRange<R>);
-		static_assert(models::BoundedRange<R>);
+		static_assert(models::View<R>, "");
+		static_assert(models::SizedRange<R>, "");
+		// FIXME: ContiguousRange
+		//static_assert(models::ContiguousRange<R>);
+		static_assert(models::RandomAccessRange<R>, "");
+		static_assert(models::CommonRange<R>, "");
 
 		CHECK(begin(r) == some_ints + 0);
 		CHECK(end(r) == some_ints + n);
 		CHECK(!empty(r));
 		CHECK(std::size_t(size(r)) == n);
-		CHECK(data(r) == some_ints);
+		// FIXME: Range acccessors
+//		CHECK(data(r) == some_ints);
+		CHECK(r.data() == some_ints);
 	}
 
-	return test_result();
+	{
+
+	}
+
+	// Deduction guides and tuple interface
+#ifdef NANO_HAVE_CPP17
+	{
+		std::vector vec{1, 2, 3, 4, 5};
+		auto r = subrange{vec};
+		auto [i, s] = r;
+		CHECK(i == vec.begin());
+		CHECK(s == vec.end());
+	}
+#endif
 }
