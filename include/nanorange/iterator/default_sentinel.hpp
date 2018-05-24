f3339e10b611e61cd12a575125ff0dd918dea3c8
Tristan Brindle <t.c.brindle@gmail.com>
2018-05-24 05:30:47 +0100

    Add default_sentinel
    Well, that was easy

---
+++ include/nanorange/iterator/default_sentinel.hpp
@@ -0,0 +1,18 @@
+// nanorange/iterator/default_sentinel.hpp
+//
+// Copyright (c) 2018 Tristan Brindle (tcbrindle at gmail dot com)
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef NANORANGE_ITERATOR_DEFAULT_SENTINEL_HPP_INCLUDED
+#define NANORANGE_ITERATOR_DEFAULT_SENTINEL_HPP_INCLUDED
+
+#include <nanorange/detail/macros.hpp>
+
+NANO_BEGIN_NAMESPACE
+
+class default_sentinel {};
+
+NANO_END_NAMESPACE
+
+#endif
