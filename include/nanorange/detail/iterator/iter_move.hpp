1dea754243125a9f769fe0ed2727362165cf77d8
Tristan Brindle <t.c.brindle@gmail.com>
2018-06-18 18:04:45 +0100

    Fix iter_move returning lvalue
    There was somewhere else I worked around this, I should go back and fix that

--- include/nanorange/detail/iterator/iter_move.hpp
+++ include/nanorange/detail/iterator/iter_move.hpp
@@ -31,19 +31,21 @@ private:
     }
 
     template <typename T>
-    static constexpr auto impl(T& t, priority_tag<1>) noexcept(
-        noexcept(std::move(*std::declval<T&>()))) -> decltype(std::move(*t))
+    static constexpr auto impl(T&& t, priority_tag<1>) noexcept(
+        noexcept(std::move(*std::declval<T&&>())))
+       -> std::enable_if_t<std::is_lvalue_reference<decltype(*std::forward<T>(t))>::value,
+                           decltype(std::move(*std::forward<T>(t)))>
     {
-        return std::move(*t);
+        return std::move(*std::forward<T>(t));
     }
 
     template <typename T>
     static constexpr auto
     impl(T&& t, priority_tag<0>)
-        noexcept(noexcept(*t))
-        -> decltype(*t)
+        noexcept(noexcept(*std::forward<T>(t)))
+        -> decltype(*std::forward<T>(t))
     {
-        return *t;
+        return *std::forward<T>(t);
     }
 
 public:
