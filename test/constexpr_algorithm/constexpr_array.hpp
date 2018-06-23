08d3a07f7adccb83392e109eda5fd95e4d9631c8
Tristan Brindle <t.c.brindle@gmail.com>
2018-06-23 18:44:14 +0100

    Test: add a constexpr std::array clone

---
+++ test/constexpr_algorithm/constexpr_array.hpp
@@ -0,0 +1,53 @@
+
+#pragma once
+
+// constexpr std::array implementation for testing
+
+#include <nanorange/algorithm/fill.hpp>
+
+template <typename T, std::size_t N>
+struct carray {
+    using value_type = T;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
+    using iterator = pointer;
+    using const_iterator = const_pointer;
+
+    constexpr reference operator[](size_type idx) { return arr_[idx]; }
+    constexpr const_reference operator[](size_type idx) const { return arr_[idx]; }
+
+    constexpr reference front() { return arr_[0]; }
+    constexpr const_reference front() const { return arr_[0]; }
+    constexpr reference back() { return arr_[N - 1]; }
+    constexpr const_reference back() const { return arr_[N - 1]; }
+
+    constexpr pointer data() { return arr_; }
+    constexpr const_pointer data() const { return arr_; }
+
+    constexpr iterator begin() { return data(); }
+    constexpr const_iterator begin() const { return data(); }
+    constexpr const_iterator cbegin() const { return begin(); }
+
+    constexpr iterator end() { return data() + N; }
+    constexpr const_iterator end() const { return data() + N; }
+    constexpr const_iterator cend() const { return end(); }
+
+    static constexpr bool empty() { return N == 0; }
+    static constexpr size_type size() { return N; }
+
+    constexpr void fill(const T& value)
+    {
+        nano::fill(*this, value);
+    }
+
+    constexpr void swap(carray& other)
+    {
+        nano::swap(arr_, other.arr_);
+    }
+
+    T arr_[N];
+};
