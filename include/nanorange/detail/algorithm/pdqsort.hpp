7e03e12c6ce7143a6a6cad30867a2f58ccce9454
Tristan Brindle <t.c.brindle@gmail.com>
2018-06-23 15:49:02 +0100

    Use the Boost.Sort version of pdqsort
    The code is identical to the standalone pdqsort.h, but under the Boost licence rather than the zlib licence

--- include/nanorange/detail/algorithm/pdqsort.hpp
+++ include/nanorange/detail/algorithm/pdqsort.hpp
@@ -1,35 +1,13 @@
 // nanorange/detail/algorithm/pqdsort.hpp
 //
+// Copyright Orson Peters 2017.
 // Copyright (c) 2018 Tristan Brindle (tcbrindle at gmail dot com)
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-// Modified from pdqsort.h by Orson Peters
-// https://github.com/orlp/pdqsort
+// Modified from Boost.Sort by Orson Peters
+// https://github.com/boostorg/sort/blob/develop/include/boost/sort/pdqsort/pdqsort.hpp
 
-/*
-   pdqsort.h - Pattern-defeating quicksort.
-
-   Copyright (c) 2015 Orson Peters
-
-   This software is provided 'as-is', without any express or implied warranty.
-   In no event will the authors be held liable for any damages arising from the
-   use of this software.
-
-   Permission is granted to anyone to use this software for any purpose,
-   including commercial applications, and to alter it and redistribute it
-   freely, subject to the following restrictions:
-
-   1. The origin of this software must not be misrepresented; you must not claim
-   that you wrote the original software. If you use this software in a product,
-   an acknowledgment in the product documentation would be appreciated but is
-   not required.
-
-   2. Altered source versions must be plainly marked as such, and must not be
-   misrepresented as being the original software.
-
-   3. This notice may not be removed or altered from any source distribution.
-*/
 
 #ifndef NANORANGE_DETAIL_ALGORITHM_PDQSORT_HPP_INCLUDED
 #define NANORANGE_DETAIL_ALGORITHM_PDQSORT_HPP_INCLUDED
