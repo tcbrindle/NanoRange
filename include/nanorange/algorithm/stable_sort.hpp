20cff5f07dd8e4f69016cb2df932889b8b51be6a
Tristan Brindle <t.c.brindle@gmail.com>
2019-01-22 03:50:47 +0000

    Implement buffered stable_sort
    Implementation from CMCSTL2 as usual.

--- include/nanorange/algorithm/stable_sort.hpp
+++ include/nanorange/algorithm/stable_sort.hpp
@@ -46,6 +46,8 @@ namespace detail {
 
 struct stable_sort_fn {
 private:
+    static constexpr int merge_sort_chunk_size = 7;
+
     template <typename I, typename Comp, typename Proj>
     static void inplace_stable_sort(I first, I last, Comp& comp, Proj& proj)
     {
@@ -61,10 +63,97 @@ private:
         }
     }
 
+    template <typename I, typename Buf, typename Comp, typename Proj>
+    static void stable_sort_adaptive(I first, I last, Buf& buf, Comp& comp, Proj& proj)
+    {
+        auto len = iter_difference_t<I>((last - first + 1) / 2);
+        auto middle = first + len;
+        if (static_cast<std::size_t>(len) > buf.capacity()) {
+            stable_sort_adaptive(first, middle, buf, comp, proj);
+            stable_sort_adaptive(middle, last, buf, comp, proj);
+        } else {
+            merge_sort_with_buffer(first, middle, buf, comp, proj);
+            merge_sort_with_buffer(middle, last, buf, comp, proj);
+        }
+        inplace_merge_fn::impl_buffered(first, middle, last,
+                                        middle - first, last - middle, buf,
+                                        comp, proj);
+    }
+
+    template <typename I, typename Buf, typename Comp, typename Proj>
+    static void merge_sort_with_buffer(I first, I last, Buf& buf, Comp& comp, Proj& proj)
+    {
+        auto len = iter_difference_t<I>(last - first);
+        auto step_size = iter_difference_t<I>(merge_sort_chunk_size);
+        chunk_insertion_sort(first, last, step_size, comp, proj);
+        if (step_size >= len) {
+            return;
+        }
+        assert(buf.empty());
+        merge_sort_loop(first, last, nano::back_inserter(buf), step_size, comp, proj);
+        step_size *= 2;
+        while (true) {
+            merge_sort_loop(buf.begin(), buf.end(), first, step_size, comp, proj);
+            step_size *= 2;
+            if (step_size >= len) {
+                buf.clear();
+                return;
+            }
+            merge_sort_loop(first, last, buf.begin(), step_size, comp, proj);
+            step_size *= 2;
+        }
+        buf.clear();
+    }
+
+    template <typename I, typename Comp, typename Proj>
+    static void chunk_insertion_sort(I first, I last, iter_difference_t<I> chunk_size,
+                                     Comp& comp, Proj& proj)
+    {
+            while (last - first >= chunk_size) {
+                detail::insertion_sort(first, first + chunk_size, comp, proj);
+                first += chunk_size;
+            }
+            detail::insertion_sort(first, last, comp, proj);
+    }
+
+    template <typename I, typename O, typename Comp, typename Proj>
+    static void merge_sort_loop(I first, I last, O result, iter_difference_t<I> step_size,
+                                Comp& comp, Proj& proj)
+    {
+        auto two_step = iter_difference_t<I>(2 * step_size);
+        while (last - first >= two_step) {
+            result = nano::merge(
+                         nano::make_move_iterator(first),
+                         nano::make_move_iterator(first + step_size),
+                         nano::make_move_iterator(first + step_size),
+                         nano::make_move_iterator(first + two_step),
+                         result, std::ref(comp), std::ref(proj), std::ref(proj)).out;
+            first += two_step;
+        }
+        step_size = nano::min(iter_difference_t<I>(last - first), step_size);
+        nano::merge(
+                nano::make_move_iterator(first),
+                nano::make_move_iterator(first + step_size),
+                nano::make_move_iterator(first + step_size),
+                nano::make_move_iterator(last),
+                result, std::ref(comp), std::ref(proj), std::ref(proj));
+    }
+
     template <typename I, typename Comp, typename Proj>
     static void impl(I first, I last, Comp& comp, Proj& proj)
     {
-        inplace_stable_sort(std::move(first), std::move(last), comp, proj);
+        auto len = last - first;
+        if (len == 0) {
+            return;
+        }
+
+        temporary_vector<iter_value_t<I>> buf(len > 256 ? len : 0);
+
+        if (buf.capacity() != 0) {
+            stable_sort_adaptive(std::move(first), std::move(last), buf, comp, proj);
+        } else {
+            inplace_stable_sort(std::move(first), std::move(last), comp, proj);
+        }
     }
 
 public:
