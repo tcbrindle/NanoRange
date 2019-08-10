0ca915741ad8067279cf73718a107df68cacf0bc
Tristan Brindle <t.c.brindle@gmail.com>
2019-08-10 16:14:06 +0100

    De-diamondify ranges::less_equal and greater_equal

--- include/nanorange/detail/functional/comparisons.hpp
+++ include/nanorange/detail/functional/comparisons.hpp
@@ -17,87 +17,6 @@ NANO_BEGIN_NAMESPACE
 
 // [range.comparisons]
 
-namespace detail {
-
-template <typename = void, typename = void>
-struct less_helper;
-
-template <>
-struct less_helper<void> {
-    template <typename T, typename U>
-    constexpr auto operator()(T&& t, U&& u) const
-        noexcept(noexcept(std::less<>{}(std::forward<T>(t), std::forward<U>(u))))
-            -> std::enable_if_t<StrictTotallyOrderedWith<T, U>, bool>
-    {
-        return std::less<>{}(std::forward<T>(t), std::forward<U>(u));
-    }
-
-    using is_transparent = std::true_type;
-};
-
-template <typename T>
-struct less_helper<T, std::enable_if_t<StrictTotallyOrdered<T>>> {
-    constexpr bool operator()(const T& t, const T& u) const
-        noexcept(noexcept(less_helper<>{}(t, u)))
-    {
-        return less_helper<>{}(t, u);
-    }
-};
-
-template <typename, typename = void>
-struct less_equal_helper;
-
-template <>
-struct less_equal_helper<void> {
-    template <typename T, typename U>
-    constexpr auto operator()(T&& t, U&& u) const
-        noexcept(noexcept(!less_helper<>{}(std::forward<U>(u),
-                                           std::forward<T>(t))))
-            -> std::enable_if_t<StrictTotallyOrderedWith<T, U>, bool>
-    {
-        return !less_helper<>{}(std::forward<U>(u), std::forward<T>(t));
-    }
-
-    using is_transparent = std::true_type;
-};
-
-template <typename T>
-struct less_equal_helper<T, std::enable_if_t<StrictTotallyOrdered<T>>> {
-    constexpr bool operator()(const T& t, const T& u) const
-        noexcept(noexcept(!less_helper<>{}(u, t)))
-    {
-        return !less_helper<>{}(u, t);
-    }
-};
-
-template <typename, typename = void>
-struct greater_equal_helper;
-
-template <>
-struct greater_equal_helper<void> {
-    template <typename T, typename U>
-    constexpr auto operator()(T&& t, U&& u) const
-        noexcept(noexcept(less_helper<>{}(std::forward<T>(t),
-                                          std::forward<U>(u))))
-            -> std::enable_if_t<StrictTotallyOrderedWith<T, U>, bool>
-    {
-        return !less_helper<>{}(std::forward<T>(t), std::forward<U>(u));
-    }
-
-    using is_transparent = std::true_type;
-};
-
-template <typename T>
-struct greater_equal_helper<T, std::enable_if_t<StrictTotallyOrdered<T>>> {
-    constexpr bool operator()(const T& t, const T& u) const
-        noexcept(noexcept(!less_helper<>{}(t, u)))
-    {
-        return !less_helper<>{}(t, u);
-    }
-};
-
-} // namespace detail
-
 struct equal_to {
     template <typename T, typename U>
     constexpr auto operator()(T&& t, U&& u) const
@@ -142,12 +61,26 @@ struct greater {
     using is_transparent = std::true_type;
 };
 
-template <typename T = void>
-struct greater_equal : detail::greater_equal_helper<T> {
+struct greater_equal {
+    template <typename T, typename U>
+    constexpr auto operator()(T&& t, U&& u) const
+    -> std::enable_if_t<StrictTotallyOrderedWith<T, U>, bool>
+    {
+        return !ranges::less{}(std::forward<T>(t), std::forward<U>(u));
+    }
+
+    using is_transparent = std::true_type;
 };
 
-template <typename T = void>
-struct less_equal : detail::less_equal_helper<T> {
+struct less_equal {
+    template <typename T, typename U>
+    constexpr auto operator()(T&& t, U&& u) const
+        -> std::enable_if_t<StrictTotallyOrderedWith<T, U>, bool>
+    {
+        return !ranges::less{}(std::forward<U>(u), std::forward<T>(t));
+    }
+
+    using is_transparent = std::true_type;
 };
 
 NANO_END_NAMESPACE
