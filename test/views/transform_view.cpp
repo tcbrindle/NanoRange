ed29515bfb9132040ce4408a6dfe37c8348bace8
Tristan Brindle <t.c.brindle@gmail.com>
2019-08-23 03:51:37 +0100

    Implement transform_view
    Fixes #3

--- test/views/transform_view.cpp
+++ test/views/transform_view.cpp
@@ -9,55 +9,73 @@
 //
 // Project home: https://github.com/caseycarter/cmcstl2
 //
-#include <stl2/view/transform.hpp>
-#include <stl2/view/iota.hpp>
-#include <stl2/detail/algorithm/count.hpp>
-#include <stl2/detail/algorithm/transform.hpp>
-#include <stl2/detail/iterator/insert_iterators.hpp>
+#include <nanorange/views/transform.hpp>
+
 #include <memory>
 #include <vector>
-#include "../simple_test.hpp"
 
-namespace ranges = __stl2;
+#include "../catch.hpp"
+#include "../test_utils.hpp"
+#include <nanorange/algorithm/count.hpp>
+#include <nanorange/algorithm/transform.hpp>
+#include <nanorange/concepts.hpp>
+#include <nanorange/views/filter.hpp>
+#include <nanorange/views/iota.hpp>
+#include <nanorange/views/reverse.hpp>
 
-namespace
-{
-	struct is_odd
-	{
-		bool operator()(int i) const
-		{
+namespace ranges = nano::ranges;
+
+namespace {
+	struct is_odd {
+		bool operator()(int i) const {
 			return (i % 2) == 1;
 		}
 	};
 }
 
-int main()
-{
+TEST_CASE("views.transform") {
 	using namespace ranges;
 
-	int rgi[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        {
+            int rgi[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
-	auto rng = rgi | view::transform(is_odd());
-	static_assert(Same<int &, decltype(*begin(rgi))>);
-	static_assert(Same<bool, decltype(*begin(rng))>);
-	static_assert(View<decltype(rng)>);
-	static_assert(SizedRange<decltype(rng)>);
-	static_assert(RandomAccessRange<decltype(rng)>);
-	CHECK_EQUAL(rng, {true, false, true, false, true, false, true, false, true, false});
+            auto rng = rgi | views::transform(is_odd());
+            static_assert(same_as<int &, decltype(*begin(rgi))>);
+            static_assert(same_as<bool, decltype(*begin(rng))>);
+            static_assert(view<decltype(rng)>);
+            static_assert(sized_range<decltype(rng)>);
+            static_assert(random_access_range<decltype(rng)>);
+            ::check_equal(rng, {true, false, true, false, true, false, true,
+                                false, true, false});
+        }
+        {
+            std::pair<int, int> rgp[] = {{1, 1}, {2, 2},  {3, 3}, {4, 4},
+                                         {5, 5}, {6, 6},  {7, 7}, {8, 8},
+                                         {9, 9}, {10, 10}};
+            auto rng2 = rgp | views::transform(&std::pair<int, int>::first);
+            static_assert(same_as<int &, decltype(*begin(rng2))>);
+            static_assert(
+                same_as<iter_value_t<iterator_t<decltype(rng2)>>, int>);
+            static_assert(same_as<decltype(iter_move(begin(rng2))), int &&>);
+            static_assert(view<decltype(rng2)>);
+            static_assert(common_range<decltype(rng2)>);
+            static_assert(sized_range<decltype(rng2)>);
+            static_assert(random_access_range<decltype(rng2)>);
+            CHECK(&*begin(rng2) == &rgp[0].first);
+            CHECK(rng2.size() == 10u);
+            ::check_equal(rng2, {1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+            ::check_equal(rng2 | views::reverse,
+                          {10, 9, 8, 7, 6, 5, 4, 3, 2, 1});
+        }
+        // https://github.com/CaseyCarter/cmcstl2/issues/262
+        {
+            auto id = [](int x) { return x; };
 
-	std::pair<int, int> rgp[] = {{1,1}, {2,2}, {3,3}, {4,4}, {5,5}, {6,6}, {7,7}, {8,8}, {9,9}, {10,10}};
-	auto rng2 = rgp | view::transform(&std::pair<int,int>::first);
-	static_assert(Same<int &, decltype(*begin(rng2))>);
-	static_assert(Same<iter_value_t<iterator_t<decltype(rng2)>>, int>);
-	static_assert(Same<decltype(iter_move(begin(rng2))), int &&>);
-	static_assert(View<decltype(rng2)>);
-	static_assert(CommonRange<decltype(rng2)>);
-	static_assert(SizedRange<decltype(rng2)>);
-	static_assert(RandomAccessRange<decltype(rng2)>);
-	CHECK_EQUAL(rng2, {1,2,3,4,5,6,7,8,9,10});
-	// CHECK_EQUAL(rng2 | views::reverse, {10,9,8,7,6,5,4,3,2,1});
-	// CHECK(&*begin(rng2) == &rgp[0].first);
-	// CHECK(rng2.size() == 10u);
+            views::iota(0, 100) | views::filter(id) | views::transform(id);
+            views::iota(0) | views::transform(id) | views::filter(id);
+            views::iota(0) | views::transform(id);
+            views::iota(0) | views::filter(id);
+            views::iota(0) | views::filter(id) | views::transform(id);
+        }
 
-	return ::test_result();
 }
