c798f39f0312bb3cabd529f5a62a969e62184685
dvirtz <dvirtz@gmail.com>
2019-10-20 17:04:27 +0300

    cleanup

--- .conan/test_package/conanfile.py
+++ .conan/test_package/conanfile.py
@@ -13,6 +13,6 @@ class NanorangeTestConan(ConanFile):
         cmake.build()
 
     def test(self):
-        assert os.path.isfile(os.path.join(self.deps_cpp_info["NanoRange"].rootpath, "licenses", "LICENSE_1_0.txt"))
+        assert os.path.isfile(os.path.join(self.deps_cpp_info["nanorange"].rootpath, "licenses", "LICENSE_1_0.txt"))
         bin_path = os.path.join("bin", "test_package")
         self.run("%s -s" % bin_path, run_environment=True)
--- conanfile.py
+++ conanfile.py
@@ -1,21 +1,19 @@
 from conans import ConanFile, CMake, tools
 import ptvsd
 
-# ptvsd.enable_attach()
-# ptvsd.wait_for_attach()
-
 class NanorangeConan(ConanFile):
-    name = "NanoRange"
+    name = "nanorange"
     version = "0.1.0"
     license = "BSL-1.0"
     author = "Tristan Brindle (tcbrindle at gmail dot com)"
     url = "https://github.com/tcbrindle/NanoRange"
     description = "Range-based goodness for C++17"
-    exports = ("../*")
-    # exports_sources = ("CMakeLists.txt", "single_include/*", "cmake/nanorangeConfig.cmake.in")
+    exports_sources = ("CMakeLists.txt", "single_include/*", "cmake/nanorangeConfig.cmake.in", "LICENSE_1_0.txt")
     generators = "cmake"
 
     def package(self):
+        ptvsd.break_into_debugger()
+        self.copy("", "", "")
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTING"] = "OFF"
         cmake.definitions["USE_SINGLE_HEADER"] = "TRUE"
